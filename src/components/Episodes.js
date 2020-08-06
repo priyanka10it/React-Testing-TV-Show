8e54097e54236447026da3fa0fa805b4e91bd4fb
priyanka10it <priyanka10.it@gmail.com>
2020-08-05 23:18:04 -0700

    MVP Completed

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -3,7 +3,7 @@ import parse from 'html-react-parser';
 
 export default function Episodes(props) {
   return (
-    <div className="episodes">
+    <div data-testid="episodes" className="episodes">
       {props.episodes.map(e => (
         <div className="episode" key={e.id}>
           {e.image && (
